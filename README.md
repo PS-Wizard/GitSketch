4b416a8c80e6c3b215a6ac0c75630ba3be9825b8
Wizard <p.swoyam.1@gmail.com>
2025-06-05 10:42:41 +0545

    tinyhttp + basic frontend

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Git Sketch
 
 #### How it works?
+
+```
+~
+
+GIT_AUTHOR_DATE="2021-12-01T12:00:00" \
+GIT_COMMITTER_DATE="2021-12-01T12:00:00" \
+git commit --allow-empty -m "batman"
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use serde::Deserialize;
+use tiny_http::{Response, Server};
+
+#[derive(Deserialize, Debug)]
+
+struct Payload {
+    dates: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::http("0.0.0.0:8080").unwrap();
+    println!("GitSketch: Up And Running at :8080");
+
+    for mut request in server.incoming_requests() {
+        if request.method().as_str() == "POST" && request.url() == "/submit" {
+            let mut content = String::new();
+            request.as_reader().read_to_string(&mut content).unwrap();
+
+            match serde_json::from_str::<Payload>(&content) {
+                Ok(data) => {
+                    println!("Got {:#?}", data.dates);
+                    let response = Response::from_string("Success: You May Push To Github Now!")
+                        .with_status_code(200);
+                    let _ = request.respond(response);
+                }
+                Err(e) => {
+                    let response =
+                        Response::from_string(format!("Invalid Json: {}", e)).with_status_code(401);
+                    let _ = request.respond(response);
+                }
+            }
+        } else {
+            let response =
+                Response::from_string("Only POST to /submit Works: {}").with_status_code(404);
+            let _ = request.respond(response);
+        }
+    }
 }
